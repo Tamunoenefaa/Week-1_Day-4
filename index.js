cad5fcf823ca6fde6b019ae52acfc7020a95af12
Tamunoenefaa Harry <tamunoenefaa@gmail.com>
2018-05-24 03:31:50 -0400

    All flicks with unordered list

---
+++ index.js
@@ -0,0 +1,37 @@
+const app = {
+    init: function(selectors) {
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+  
+      document
+        .querySelector(selectors.formSelector)
+        .addEventListener('submit', ev => {
+          ev.preventDefault()
+          this.handleSubmit(ev)
+        })
+    },
+  
+    renderListItem: function(flick) {
+      const item = document.createElement('li')
+      item.textContent = flick.name
+      return item
+    },
+  
+    handleSubmit: function(ev) {
+      const f = ev.target
+      const flick = {
+        id: ++this.max,
+        name: f.flickName.value,
+      }
+  
+      const item = this.renderListItem(flick)
+      this.list.appendChild(item)
+  
+      f.reset()
+    },
+  }
+  
+  app.init({
+    formSelector: '#flickForm',
+    listSelector: '#flickList',
+  })
